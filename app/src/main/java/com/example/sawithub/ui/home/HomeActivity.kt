ddf0ac777e8985f43393427234e16d2b4605be19
nandaditra <nandaditra56@gmail.com>
2023-05-31 16:11:18 +0700

    make some changes

--- app/src/main/java/com/example/sawithub/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/sawithub/ui/home/HomeActivity.kt
@@ -21,13 +21,10 @@ class HomeActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_home)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_scan, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
--- app/src/main/java/com/example/sawithub/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/sawithub/ui/home/HomeFragment.kt
@@ -7,14 +7,16 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.sawithub.R
 import com.example.sawithub.databinding.FragmentHomeBinding
+import com.smarteist.autoimageslider.SliderView
 
 class HomeFragment : Fragment() {
 
+    lateinit var imageUrlSource: ArrayList<Int>
+    lateinit var sliderBannerView: SliderView
+    lateinit var bannerSliderAdapter: BannerSliderAdapter
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,17 +24,27 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
+        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
         val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        homeViewModel.text.observe(viewLifecycleOwner) { textView.text = it }
+        bannerSlider()
         return root
+    }
+
+    private fun bannerSlider() {
+        sliderBannerView = binding.bannerSlider
+        imageUrlSource = ArrayList<Int>()
+        imageUrlSource.add(R.drawable.banner_1)
+        imageUrlSource.add(R.drawable.banner_2)
+        imageUrlSource.add(R.drawable.banner_3)
+        bannerSliderAdapter = BannerSliderAdapter(imageUrlSource)
+        sliderBannerView.autoCycleDirection = SliderView.LAYOUT_DIRECTION_RTL
+        sliderBannerView.setSliderAdapter(bannerSliderAdapter)
+        sliderBannerView.scrollTimeInSec = 3
+        sliderBannerView.isAutoCycle = true
+        sliderBannerView.startAutoCycle()
     }
 
     override fun onDestroyView() {
