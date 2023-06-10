e6ec99160e7dae909c4e82f2ee883dc9a3e2f6dd
nandaditra <nandaditra56@gmail.com>
2023-06-10 10:58:28 +0700

    remove code is not used

---
+++ app/src/main/java/com/example/sawithub/ui/hasilScan/HasilScanActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sawithub.ui.hasilScan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HasilScanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hasil_scan)
+    }
+}
--- app/src/main/java/com/example/sawithub/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/sawithub/ui/home/HomeActivity.kt
@@ -6,10 +6,6 @@ import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.example.sawithub.R
 import com.example.sawithub.databinding.ActivityHomeBinding
 import com.example.sawithub.ui.dashboard.DashboardFragment
@@ -26,19 +22,6 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        val navView: BottomNavigationView = binding.navView
-//        val navController = findNavController(R.id.nav_host_fragment_activity_home)
-//        val appBarConfiguration = AppBarConfiguration(
-//            setOf(
-//                R.id.navigation_home,
-//                R.id.navigation_scan,
-//                R.id.navigation_dashboard,
-//                R.id.navigation_profile
-//            )
-//        )
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//        navView.setupWithNavController(navController)
-//        loadFragment(HomeFragment())
         bottomNav = binding.navView as BottomNavigationView
         bottomNav.setOnItemSelectedListener {
             when (it.itemId) {
